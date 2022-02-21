660ab2b456b440e593e203df7335f2eef80b8318
Nnaoyy <yoann.jamain@hotmail.fr>
2022-02-21 10:43:23 +0100

    p6

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const saucesRoutes = require ('./routes/sauces');
+const userRoutes = require ('./routes/user')
+
+mongoose.connect('mongodb+srv://yoann:1234@cluster0.h2qnd.mongodb.net/test?retryWrites=true&w=majority',
+  {  useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+/*Permet l'accés à l'api depuis n'import quelle origine*/
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(express.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
